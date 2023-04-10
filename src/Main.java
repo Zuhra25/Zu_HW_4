354cc02c50249d534b9c28bec8e1d0c92c0895ae
ZU <zshaihova@gmail.com>
2023-04-10 21:29:29 +0300

    задача 1

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,24 @@
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-
-            System.out.println("111" );
+        task1();
+        task2();
+    }
+        public static void task1 () {
+            System.out.println("Задача 1:");
+            int age = 10;
+            var message1 = "он совершеннолетний";
+            var message2 = "он не достиг совершеннолетия, нужно немного подождать";
+            if (age >= 18) {
+                System.out.println("Если возраст человека равен " + age + ", то " + message1);
+            } else {
+                System.out.println("Если возраст человека равен " + age + ", то " + message2);
+            }
         }
+
+        public static void task2 () {
+            System.out.println("Задача 2:");
+                System.out.println("Если возраст человека равен");
+            }
+
 }
